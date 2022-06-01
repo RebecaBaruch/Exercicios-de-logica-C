06cfeac20c4edf7fb5fd4eecadc8591bee6486ed
Rebeca Baruch <rebecabaruch@outlook.com>
2022-06-01 15:22:48 -0300

    fiz o ex06

---
+++ src/ex06.c/ex06.c
@@ -0,0 +1,14 @@
+int main(){
+
+    int numberH = 8, i = 1, product;
+
+    printf("Se liga na tabuada do 8: \n");
+
+    while(i <= 10){
+
+        product = numberH * i;
+        printf("8 x %d = %d\n", i, product);
+
+        i++;
+    }
+}
