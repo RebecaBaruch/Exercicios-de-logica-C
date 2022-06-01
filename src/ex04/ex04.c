4049195517f77bb905a885073d7e5a0964057911
Rebeca Baruch <rebecabaruch@outlook.com>
2022-06-01 15:01:05 -0300

    terminei o ex04

---
+++ src/ex04/ex04.c
@@ -0,0 +1,28 @@
+int main(){
+
+    int i = 1, divider, n, count = 0;
+    double rest;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    divider = n;
+
+    printf("Divisores: \n");
+
+    while(divider >= 1){
+        
+        rest = (n % divider);
+
+        if(rest == 0){
+            printf("%d\n", divider);
+            count++;    
+        }
+
+        divider--;
+        
+        i++;
+    }
+
+    printf("Quantidade de divisores: %d", count);
+}
